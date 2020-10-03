2c468ae06d8fb7ae91b752bc1351534f06f3293b
Mahmoud Adnan <mahmoud.saied.adnan@gmail.com>
2020-10-04 00:13:46 +0200

    disable back button

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -17,177 +17,182 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      bottom: false,
-      child: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 30,
-                  ),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Hi Henrietta',
-                            style: titleStyle(
-                                fontFamily: boldFontFamily, fontSize: 20)),
-                        SizedBox(height: 10),
-                        Text('Nina’s Profile',
-                            style: titleStyle(
-                                fontFamily: mediumFontFamily, fontSize: 18)),
-                      ],
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: SafeArea(
+        bottom: false,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 30,
                     ),
-                  ),
-                  Icon(Icons.search, color: Colors.white, size: 30),
-                  SizedBox(width: 20),
-                  Icon(Icons.notifications, color: Colors.white, size: 30),
-                ],
-              ),
-              SizedBox(height: 20),
-              // subaccounts ..
-              Row(
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 24,
-                    backgroundColor: Colors.white,
-                    child: CircleAvatar(
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Hi Henrietta',
+                              style: titleStyle(
+                                  fontFamily: boldFontFamily, fontSize: 20)),
+                          SizedBox(height: 10),
+                          Text('Nina’s Profile',
+                              style: titleStyle(
+                                  fontFamily: mediumFontFamily, fontSize: 18)),
+                        ],
+                      ),
+                    ),
+                    Icon(Icons.search, color: Colors.white, size: 30),
+                    SizedBox(width: 20),
+                    Icon(Icons.notifications, color: Colors.white, size: 30),
+                  ],
+                ),
+                SizedBox(height: 20),
+                // subaccounts ..
+                Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 24,
+                      backgroundColor: Colors.white,
+                      child: CircleAvatar(
+                          radius: 22,
+                          backgroundColor: Color.fromRGBO(0, 54, 115, 1),
+                          child: Icon(
+                            Icons.add,
+                            color: Colors.white,
+                          )),
+                    ),
+                    SizedBox(width: 10),
+                    CircleAvatar(
+                      radius: 24,
+                      backgroundColor: Colors.white,
+                      child: CircleAvatar(
                         radius: 22,
                         backgroundColor: Color.fromRGBO(0, 54, 115, 1),
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.white,
-                        )),
-                  ),
-                  SizedBox(width: 10),
-                  CircleAvatar(
-                    radius: 24,
-                    backgroundColor: Colors.white,
-                    child: CircleAvatar(
-                      radius: 22,
-                      backgroundColor: Color.fromRGBO(0, 54, 115, 1),
-                      child: Text('NH',
+                        child: Text('NH',
+                            style: titleStyle(
+                                fontFamily: boldFontFamily, fontSize: 18)),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    CircleAvatar(
+                      radius: 24,
+                      backgroundColor: Colors.white70,
+                      child: Text('ML',
                           style: titleStyle(
-                              fontFamily: boldFontFamily, fontSize: 18)),
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  CircleAvatar(
-                    radius: 24,
-                    backgroundColor: Colors.white70,
-                    child: Text('ML',
-                        style: titleStyle(
-                            fontFamily: boldFontFamily,
-                            fontSize: 18,
-                            color: Color.fromRGBO(163, 172, 194, 1))),
-                  )
-                ],
-              ),
-              SizedBox(height: 20),
-              // health title
-              Text(
-                'How is your Health ?',
-                style: titleStyle(fontFamily: boldFontFamily, fontSize: 19),
-              ),
-              SizedBox(height: 20),
-              // health details ..
-              Row(
-                children: <Widget>[
-                  Expanded(
-                      child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text('Blood pressure',
-                                  style:
-                                      titleStyle(fontFamily: mediumFontFamily)),
-                              SizedBox(height: 10),
-                              Text('120/70',
-                                  style: titleStyle(
-                                      fontFamily: boldFontFamily,
-                                      fontSize: 18)),
-                              SizedBox(height: 10),
-                              Text('Normal',
-                                  style: titleStyle(
-                                      fontFamily: semiBoldFontFamily)),
-                            ],
-                          ),
-                        ),
-                        Image.asset(Blood_Pressure)
-                      ],
-                    ),
-                  )),
-                  SizedBox(width: 10),
-                  Expanded(
-                      child: Container(
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(233, 178, 9, 1),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text('Heart Rate',
-                                  style:
-                                      titleStyle(fontFamily: mediumFontFamily)),
-                              SizedBox(height: 10),
-                              Text('120 bpm',
-                                  style: titleStyle(
-                                      fontFamily: boldFontFamily,
-                                      fontSize: 18)),
-                              SizedBox(height: 10),
-                              Text('Normal',
-                                  style: titleStyle(
-                                      fontFamily: semiBoldFontFamily)),
-                            ],
-                          ),
-                        ),
-                        Image.asset(Heartbeat)
-                      ],
-                    ),
-                  ))
-                ],
-              ),
-              SizedBox(height: 20),
-              // Upcoming reminder ..
-              MediciationSection(medicationList: [],),
-              // hospitals list ..
-              Text('Hospitals',
-                  style: titleStyle(
-                      fontFamily: boldFontFamily,
-                      fontSize: 22,
-                      color: defaultBackgroundColor)),
-              Container(
-                width: double.infinity,
-                height: 200,
-                child: ListView.builder(
-                  itemCount: 4,
-                  itemBuilder: (BuildContext context, int index) {
-                    return HospitalCell();
-                  },
+                              fontFamily: boldFontFamily,
+                              fontSize: 18,
+                              color: Color.fromRGBO(163, 172, 194, 1))),
+                    )
+                  ],
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                // health title
+                Text(
+                  'How is your Health ?',
+                  style: titleStyle(fontFamily: boldFontFamily, fontSize: 19),
+                ),
+                SizedBox(height: 20),
+                // health details ..
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text('Blood pressure',
+                                    style: titleStyle(
+                                        fontFamily: mediumFontFamily)),
+                                SizedBox(height: 10),
+                                Text('120/70',
+                                    style: titleStyle(
+                                        fontFamily: boldFontFamily,
+                                        fontSize: 18)),
+                                SizedBox(height: 10),
+                                Text('Normal',
+                                    style: titleStyle(
+                                        fontFamily: semiBoldFontFamily)),
+                              ],
+                            ),
+                          ),
+                          Image.asset(Blood_Pressure)
+                        ],
+                      ),
+                    )),
+                    SizedBox(width: 10),
+                    Expanded(
+                        child: Container(
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(233, 178, 9, 1),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text('Heart Rate',
+                                    style: titleStyle(
+                                        fontFamily: mediumFontFamily)),
+                                SizedBox(height: 10),
+                                Text('120 bpm',
+                                    style: titleStyle(
+                                        fontFamily: boldFontFamily,
+                                        fontSize: 18)),
+                                SizedBox(height: 10),
+                                Text('Normal',
+                                    style: titleStyle(
+                                        fontFamily: semiBoldFontFamily)),
+                              ],
+                            ),
+                          ),
+                          Image.asset(Heartbeat)
+                        ],
+                      ),
+                    ))
+                  ],
+                ),
+                SizedBox(height: 20),
+                // Upcoming reminder ..
+                MediciationSection(
+                  medicationList: [],
+                ),
+                // hospitals list ..
+                Text('Hospitals',
+                    style: titleStyle(
+                        fontFamily: boldFontFamily,
+                        fontSize: 22,
+                        color: defaultBackgroundColor)),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  child: ListView.builder(
+                    itemCount: 4,
+                    itemBuilder: (BuildContext context, int index) {
+                      return HospitalCell();
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/Screens/landing_screen.dart
+++ lib/Screens/landing_screen.dart
@@ -17,86 +17,92 @@ class _LandingScreenState extends State<LandingScreen> {
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: Constants.textDirection,
-      child: new Scaffold(
-        body: Container(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            color: Color.fromRGBO(245, 248, 252, 1),
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  height: 250,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage(Login_background), fit: BoxFit.cover),
-                  ),
-                ),
-                // HospitalDetailsScreen()
-                HomeScreen()
-              ],
-            ),
-          ),
-        ),
-        floatingActionButton: Container(
-          height: 70,
-          width: 70,
-          margin: EdgeInsets.only(top: 30),
-          child: FloatingActionButton(
-            onPressed: () {},
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-            elevation: 4.0,
-            backgroundColor: defaultBackgroundColor,
-          ),
-        ),
-        bottomNavigationBar: Container(
-          child: Container(
-            color: Color.fromRGBO(245, 248, 252, 1),
+      child: WillPopScope(
+        onWillPop: () async => false,
+        child: new Scaffold(
+          body: Container(
             child: Container(
-              height: 70,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(20),
-                    topLeft: Radius.circular(20),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.grey[300],
-                        spreadRadius: 2,
-                        blurRadius: 10)
-                  ]),
-              child: Row(
+              height: MediaQuery.of(context).size.height,
+              color: Color.fromRGBO(245, 248, 252, 1),
+              child: Stack(
                 children: <Widget>[
-                  Expanded(
-                      child: IconButton(
-                    icon: Image.asset(Home_Active),
-                    iconSize: 140,
-                  )),
-                  Expanded(
-                      child: IconButton(
-                    icon: Image.asset(
-                      Appointement_inactive,
+                  Container(
+                    height: 250,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(Login_background),
+                          fit: BoxFit.cover),
                     ),
-                    iconSize: 140,
-                  )),
-                  Expanded(child: new Text('')),
-                  Expanded(
-                      child: IconButton(
-                          icon: Image.asset(History_inctive), iconSize: 140)),
-                  Expanded(
-                      child: IconButton(
-                          icon: Image.asset(Medicine_inctive), iconSize: 140)),
+                  ),
+                  // HospitalDetailsScreen()
+                  HomeScreen()
                 ],
-                // unselectedItemColor: Colors.grey,
-                // selectedItemColor: Colors.black,
-                // showUnselectedLabels: true,
               ),
             ),
           ),
+          floatingActionButton: Container(
+            height: 70,
+            width: 70,
+            margin: EdgeInsets.only(top: 30),
+            child: FloatingActionButton(
+              onPressed: () {},
+              tooltip: 'Increment',
+              child: Icon(Icons.add),
+              elevation: 4.0,
+              backgroundColor: defaultBackgroundColor,
+            ),
+          ),
+          bottomNavigationBar: Container(
+            child: Container(
+              color: Color.fromRGBO(245, 248, 252, 1),
+              child: Container(
+                height: 70,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(20),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey[300],
+                          spreadRadius: 2,
+                          blurRadius: 10)
+                    ]),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: IconButton(
+                      icon: Image.asset(Home_Active),
+                      iconSize: 140,
+                    )),
+                    Expanded(
+                        child: IconButton(
+                      icon: Image.asset(
+                        Appointement_inactive,
+                      ),
+                      iconSize: 140,
+                    )),
+                    Expanded(child: new Text('')),
+                    Expanded(
+                        child: IconButton(
+                            icon: Image.asset(History_inctive), iconSize: 140)),
+                    Expanded(
+                        child: IconButton(
+                            icon: Image.asset(Medicine_inctive),
+                            iconSize: 140)),
+                  ],
+                  // unselectedItemColor: Colors.grey,
+                  // selectedItemColor: Colors.black,
+                  // showUnselectedLabels: true,
+                ),
+              ),
+            ),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
   }
